5abe9a3f16e4390d8f3058e6ff3e70db2e146f5b
tolotrasam <tolotrasam@gmail.com>
2016-06-01 21:08:02 +0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,29 +1,35 @@
 <?php
 //this is an old fashion of defining strings, dont use it anymore (mysql)
 
-$servername = "localhost";
-$username = "root";
-$password = "1234";
-$database = "alui";
-$conn = new mysqli($servername, $username, $password);
+//$servername = "localhost";
+//$username = "root";
+//$password = "1234";
+//$database = "alui";
+//$conn = new mysqli($servername, $username, $password);
 // Create a Database
-$sql = "CREATE DATABASE $database ";
-if ($conn->query($sql) === TRUE) {
-    echo "Database created successfully";
-} else {
+//$sql = "CREATE DATABASE $database ";
+//if ($conn->query($sql) === TRUE) {
+  //  echo "Database created successfully";
+//} else {
     // echo "Error creating '$database': " . $conn->error;
-}
+//}
 // closing connection, cause we need to connect to the new database
-mysqli_close($conn);
+//mysqli_close($conn);
 // new connection to the database
 	
 // $servername = "sql3.freemysqlhosting.net";
 // $username = "sql3119062";
 // $password = "M3SFZ429pu";
 // $database = "sql3119062";
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+$conn = new mysqli($server, $username, $password, $db);
 
 // Create connection we use $conn instead of $link
-$conn = new mysqli($servername, $username, $password, $database);
+//$conn = new mysqli($servername, $username, $password, $database);
 // Check connection
 if (mysqli_connect_errno())
   {
